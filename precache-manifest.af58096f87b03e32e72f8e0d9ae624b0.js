0666b32e843f5c0b4e9940d60b631ae264556541
Z1maka <zhenya.masnyy@gmail.com>
2019-08-22 23:29:16 +0300

    Updates

---
+++ precache-manifest.af58096f87b03e32e72f8e0d9ae624b0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "24a7b070719086734dc6c995fedcb7d6",
+    "url": "/XLSXParser/index.html"
+  },
+  {
+    "revision": "fb7a54b28e7bcf76fa7e",
+    "url": "/XLSXParser/static/css/main.e52ebf3a.chunk.css"
+  },
+  {
+    "revision": "da3158bd0b89784b0562",
+    "url": "/XLSXParser/static/js/2.e413bcc8.chunk.js"
+  },
+  {
+    "revision": "fb7a54b28e7bcf76fa7e",
+    "url": "/XLSXParser/static/js/main.76ea788f.chunk.js"
+  },
+  {
+    "revision": "592a5b4c3382884f1630",
+    "url": "/XLSXParser/static/js/runtime~main.c0c481ec.js"
+  },
+  {
+    "revision": "781f263763cac1223f6aa4f9f86edd36",
+    "url": "/XLSXParser/static/media/bounce.781f2637.png"
+  },
+  {
+    "revision": "743fdc7d35c0d1e85fc8e13e0cbbc369",
+    "url": "/XLSXParser/static/media/server.743fdc7d.png"
+  }
+]);
